1ad8f7712bd6b862ac776b3c5c6e33cc6a6e1bad
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2022-09-02 18:01:25 +0100

    Added the feature tests for checking super admin authentication.

---
+++ tests/Feature/SuperAdmin/AuthenticationTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature\SuperAdmin;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+    }
+
+    public function superAdmin()
+    {
+        return User::where('username', 'Ojsholly')->first();
+    }
+
+    public function test_super_admin_can_login_with_email()
+    {
+        $this->withoutExceptionHandling();
+
+        $superAdmin = $this->superAdmin();
+
+        $response = $this->post(route('verify-credentials'), [
+            'identifier' => $superAdmin->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertRedirect(route('super-admin.dashboard'));
+
+        $this->assertAuthenticatedAs($superAdmin);
+    }
+
+public function test_super_admin_can_login_with_username()
+    {
+        $this->withoutExceptionHandling();
+
+        $superAdmin = $this->superAdmin();
+
+        $response = $this->post(route('verify-credentials'), [
+            'identifier' => $superAdmin->username,
+            'password' => 'password',
+        ]);
+
+        $response->assertRedirect(route('super-admin.dashboard'));
+
+        $this->assertAuthenticatedAs($superAdmin);
+    }
+}
