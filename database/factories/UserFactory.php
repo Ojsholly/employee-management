4720669eb6b034337045e848b836871774d46585
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2022-09-03 08:28:10 +0100

    Added the feature tests for the admin management module by the super admin.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -59,7 +59,7 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'first_name' => fake()->firstName,
             'last_name' => fake()->lastName,
-            'username' => fake()->unique(true)->userName,
+            'username' => fake()->unique(true)->userName(),
             'email' => fake()->unique(true)->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
